74f23a0f9e2cdd9a75ebd523bc4b6dda76b4905f
Liam Stanley <me@liamstanley.io>
2021-04-04 04:39:14 -0400

    add models.Omit for json marshalling

--- devenv/clean.sh
+++ devenv/clean.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
-if [ "$(basename $PWD)" != "spectrograph" ];then
+BASE="$(basename $PWD)"
+
+if [ "$BASE" != "spectrograph" ];then
     >&2 echo "error: run $0 from the root of the project"
     exit 1
 fi
 
+set -x
 docker-compose \
-    --project-name "$(basename $PWD)" \
+    --project-name "$BASE" \
     --file ./devenv/docker-compose.yml \
     rm --stop -v --force
--- pkg/models/validate.go
+++ pkg/models/validate.go
@@ -7,3 +7,18 @@ package models
 import "gopkg.in/go-playground/validator.v9"
 
 var validate = validator.New()
+
+// Omit can be used to ignore fields on a given struct, when wrapping the struct.
+// For example, given the following structs:
+// type User struct {
+//     Email    string `json:"email"`
+//     Password string `json:"password"`
+//     // many more fields…
+// }
+// type PublicUser struct {
+//     *User
+//     Token    string `json:"token"`
+//     Password models.Omit   `json:"password,omitempty"`
+// }
+// Using PublicUser{User}, would ensure Password isn't exposed.
+type Omit *struct{}
